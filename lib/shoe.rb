94ce6115261d3fcf2eeb8dd4751d18405d60b901
Michael Fahim <michael_fahim@yahoo.com>
2019-12-14 18:46:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,25 +10,7 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-  attr_accessor :author, :page_count  # remove the attr_accessor for genre
-  attr_reader :title, :genre  # add an attr_reader for genre
-
-  GENRES = []
-
-  def initialize(title)
-    @title = title
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-
-  # create the writer for genre and add the logic for the class constant
   def brand=(brand)
     @brand = brand
-    BRANDS << brand
-  end
-
-
-end
+    BRAND << brand
+  endend
